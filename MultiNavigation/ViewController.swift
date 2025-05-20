c33a2330cded23c8396c256ae69b86f66ab2be4b
Shivam Gupta <shivam.gupta@rksv.in>
2025-05-20 17:42:29 +0530

    Initial Commit

---
+++ MultiNavigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiNavigation
+//
+//  Created by upstox on 20/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MultiNavigationTests/MultiNavigationTests.swift
@@ -0,0 +1,17 @@
+//
+//  MultiNavigationTests.swift
+//  MultiNavigationTests
+//
+//  Created by upstox on 20/05/25.
+//
+
+import Testing
+@testable import MultiNavigation
+
+struct MultiNavigationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
